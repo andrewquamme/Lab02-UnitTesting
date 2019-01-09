fd6f5332f5d00596af11c4497499a1f328aa6418
andrewquamme <andrewquamme@gmail.com>
2019-01-08 21:56:48 -0800

    scaffolds ATM menu system

--- Lab02-UnitTesting/Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Lab02-UnitTesting/Program.cs
@@ -6,7 +6,43 @@ namespace Lab02_UnitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Generic Bank's ATM");
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Make a Selction From the Following Menu:");
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Make a Withdrawal");
+            Console.WriteLine("3. Make a Deposit");
+            Console.WriteLine("4. Exit");
+            string menuSelection = Console.ReadLine();
+            int selection = Convert.ToInt32(menuSelection);
+
+            switch (selection)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void AnotherTransaction()
+        {
+            Console.WriteLine("Would you like to make another transaction? Y/N");
+            string selection = Console.ReadLine();
+            if (selection == "Y")
+            {
+                MainMenu();
+            }
         }
     }
 }
