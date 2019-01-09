b5caaeadb6e785bdaf6569750028b5ef8c8dd9e0
andrewquamme <andrewquamme@gmail.com>
2019-01-09 12:12:06 -0800

    Codes deposit logic and tests

--- Lab02-UnitTesting/Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Lab02-UnitTesting/Program.cs
@@ -2,63 +2,88 @@
 
 namespace Lab02_UnitTesting
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            double balance = 5000;
             Console.WriteLine("Welcome to Generic Bank's ATM");
-            MainMenu();
+            MainMenu(balance);
         }
 
-        static void MainMenu()
+        static void MainMenu(double balance)
         {
-            double balance = 5000;
             Console.WriteLine("Make a Selction From the Following Menu:");
             Console.WriteLine("1. Check Balance");
             Console.WriteLine("2. Make a Withdrawal");
             Console.WriteLine("3. Make a Deposit");
             Console.WriteLine("4. Exit");
-            string menuSelection = Console.ReadLine();
-            int selection = 0;
-            try
-            {
-                selection = Convert.ToInt32(menuSelection);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid Selection");
-                MainMenu();
-            }
-            finally
-            {
-                selection = Convert.ToInt32(menuSelection);
-            }
+            string userInput = Console.ReadLine();
+            double selection = CheckForNumbers(userInput, balance);
+            
             switch (selection)
             {
                 case 1:
                     Console.WriteLine($"Your current balance is ${balance}");
-                    AnotherTransaction();
+                    AnotherTransaction(balance);
                     break;
                 case 2:
+                    AnotherTransaction(balance);
                     break;
                 case 3:
+                    Console.Write("Enter deposit amount: ");
+                    string amount = Console.ReadLine();
+                    double deposit = CheckForNumbers(amount, balance);
+                    balance = MakeDeposit(balance, deposit);
+                    AnotherTransaction(balance);
                     break;
                 case 4:
                     Environment.Exit(0);
                     break;
                 default:
+                    Console.WriteLine("***Please make a valid selection.***");
+                    MainMenu(balance);
                     break;
             }
         }
 
-        static void AnotherTransaction()
+        static double CheckForNumbers(string input, double balance)
+        {
+            double number = 0;
+            try
+            {
+                number = Convert.ToInt32(input);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("***An error has occurred. Please try again***");
+                MainMenu(balance);
+            }
+            return number;
+        }
+
+        static void AnotherTransaction(double balance)
         {
             Console.WriteLine("Would you like to make another transaction? Y/N");
             string selection = Console.ReadLine();
-            if (selection == "Y")
+            if (selection.ToUpper() == "Y")
             {
-                MainMenu();
+                MainMenu(balance);
             }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        public static double MakeDeposit(double balance, double deposit)
+        {
+            if(deposit < 0)
+            {
+                Console.WriteLine("***Please enter positive numbers only");
+                MainMenu(balance);
+            }
+            return balance + deposit;
         }
     }
 }
--- Lab02-UnitTesting/XUnitTestProject1/UnitTest1.cs
+++ Lab02-UnitTesting/XUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using Lab02_UnitTesting;
 
 namespace XUnitTestProject1
 {
-    public class UnitTest1
+    public class DepositTests
     {
         [Fact]
-        public void Test1()
+        public void CanMakeDeposit()
         {
+            Assert.Equal(5100, Program.MakeDeposit(5000, 100));
+        }
 
+        [Fact]
+        public void CannotMakeNegativeDeposit()
+        {
+            Assert.Equal(5000, Program.MakeDeposit(5000, -100));
         }
     }
 }
